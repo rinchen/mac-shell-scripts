a97768cc10721020b6e89547f07893d5b772bbc3
Joey STANFORD <joey@stan4d.net>
2017-02-25 16:07:06 -0700

    Fix typo

--- installhpm.sh
+++ installhpm.sh
@@ -4,5 +4,5 @@ hpm install hyperterm-tab-icons
 hpm install hyperterm-paste
 hpm install hyperterm-focus-reporting
 hpm install hyperterm-cursor
-hpm instlal hyperterm-mactabs
+hpm install hyperterm-mactabs
 hpm install hyperlinks
