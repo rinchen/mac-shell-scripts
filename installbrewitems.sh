adef58d1af3bfcfdf2c355681c1118aedc0b471c
Joey STANFORD <joey@stan4d.net>
2017-06-29 08:43:24 -0600

    more mac brewing

---
+++ installbrewitems.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+brew install curl git wget tmux wine magic-wormhole lua luajit node ssh-copy-id vim zsh mobile-shell sshuttle vimpager mtr stormssh
--- installcasks.sh
+++ installcasks.sh
@@ -1,3 +1,3 @@
-#!/bin/eh
+#!/bin/sh
 brew tap caskroom/cask
 brew cask install adium arduino etrecheck jdownloader namebench riot stellarium adobe-acrobat-reader ccleaner gimp keka onyx sdformatter viber adobe-air chicken istumbler libreoffice peerio skype angry-ip-scanner discord java mac-informer resilio-sync steam google-chrome firefox thunderbird minecraft
