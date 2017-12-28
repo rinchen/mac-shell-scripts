299a4001cb5401fd547f57ccca69c55ca923ee72
Joey STANFORD <rinchen@users.noreply.github.com>
2017-12-28 11:54:04 -0700

    out with resilio

--- installcasks.sh
+++ installcasks.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 brew tap caskroom/cask
-brew cask install etrecheck adobe-acrobat-reader ccleaner gimp keka onyx sdformatter adobe-air istumbler libreoffice skype angry-ip-scanner discord java mac-informer resilio-sync steam google-chrome firefox thunderbird minecraft etcher macdown github-desktop zerotier-one telegram atom
+brew cask install etrecheck adobe-acrobat-reader ccleaner gimp keka onyx sdformatter adobe-air istumbler libreoffice skype angry-ip-scanner discord java mac-informer steam google-chrome firefox thunderbird minecraft etcher macdown github-desktop zerotier-one telegram atom
