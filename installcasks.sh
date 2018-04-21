27e0533f8aeee106b1bcb24d90f1c050331a0b97
Joey STANFORD <rinchen@users.noreply.github.com>
2018-04-21 12:40:40 -0600

    updates

--- installcasks.sh
+++ installcasks.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 brew tap caskroom/cask
-brew cask install adobe-acrobat-reader beaker-browser firefox istumbler mac-informer obs skype webtorrent adobe-air ccleaner gimp java macdown onyx steam zerotier-one angry-ip-scanner discord minecraft arduino etcher github-desktop keka monolingual riot telegram atom etrecheck google-chrome libreoffice mumble sdformatter thunderbird inkscape android-studio origin adapter appcleaner handbrake
+brew cask install adobe-acrobat-reader beaker-browser firefox istumbler mac-informer obs skype webtorrent adobe-air ccleaner gimp java macdown onyx steam zerotier-one angry-ip-scanner discord minecraft arduino etcher keka monolingual telegram atom etrecheck google-chrome libreoffice sdformatter thunderbird origin adapter appcleaner handbrake resilio-sync
