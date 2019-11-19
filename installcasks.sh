f2658db8e4d52deae97a9a4321eea544e82fb385
Joey STANFORD <rinchen@users.noreply.github.com>
2019-11-19 16:25:53 -0700

    renamed etcher

--- installcasks.sh
+++ installcasks.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 brew tap caskroom/cask
-brew cask install adobe-acrobat-reader firefox latest skype webtorrent gimp macdown onyx steam zerotier-one angry-ip-scanner discord arduino etcher keka monolingual telegram atom etrecheckpro google-chrome libreoffice sdformatter thunderbird origin adapter appcleaner handbrake resilio-sync
+brew cask install adobe-acrobat-reader firefox latest skype webtorrent gimp macdown onyx steam zerotier-one angry-ip-scanner discord arduino balenaetcher keka monolingual telegram atom etrecheckpro google-chrome libreoffice sdformatter thunderbird origin adapter appcleaner handbrake resilio-sync
