3479686cd6f010ee274025e6080ad9ad69e6764c
Joey STANFORD <rinchen@users.noreply.github.com>
2019-11-19 18:15:05 -0700

    added github

--- installcasks.sh
+++ installcasks.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 brew tap caskroom/cask
-brew cask install adobe-acrobat-reader firefox latest skype webtorrent gimp macdown onyx steam zerotier-one angry-ip-scanner discord arduino balenaetcher keka monolingual telegram atom etrecheckpro google-chrome libreoffice sdformatter thunderbird origin adapter appcleaner handbrake
+brew cask install adobe-acrobat-reader firefox latest skype webtorrent gimp macdown onyx steam zerotier-one angry-ip-scanner discord arduino balenaetcher keka monolingual telegram atom etrecheckpro google-chrome libreoffice sdformatter thunderbird origin adapter appcleaner handbrake github
