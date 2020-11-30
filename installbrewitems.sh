4061e5dd698f364185593461cd7b3a7ce5539c66
Joey STANFORD <rinchen@users.noreply.github.com>
2020-11-30 15:43:30 -0700

    remove vimpager and csshx (broken, use i2cssh instead)

--- installbrewitems.sh
+++ installbrewitems.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-brew install curl git wget tmux winetricks lua luajit node ssh-copy-id vim zsh zsh-completions mosh sshuttle vimpager mtr stormssh tldr hub python csshx curl stormssh-completion
+brew install curl git wget tmux winetricks lua luajit node ssh-copy-id vim zsh zsh-completions mosh sshuttle mtr stormssh tldr hub python curl stormssh-completion
